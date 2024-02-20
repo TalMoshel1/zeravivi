189a80cd6b1866b1888b5e4a173e0d261a6a8dd9
unknown <talmoshel444@gmail.com>
2024-02-20 18:37:07 +0200

    1push

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,9 +2,11 @@ import React from "react";
 import { GiHamburgerMenu } from "react-icons/gi";
 import "../components-css/Navbar.css";
 const Navbar = (props) => {
+
+  console.log('fgh')
+
   return (
 
-    console.log('fgh')
     <nav>
       <div className="icon-container">
         <GiHamburgerMenu size="50" />
